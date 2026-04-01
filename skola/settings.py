c6fb83d44de903e0453638f676677685b97ef9ad
skutas23 <skutas23@Z00P1-04>
2026-04-01 09:02:07 +0200

    e

--- skola/settings.py
+++ skola/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-jf)ytxxq6z%51erh#yr#meg)(*h=c($m7@bocae8-j-))nxsa='
+SECRET_KEY = os.environ.get('SECRET_KEY', 'your-local-insecure-key-here')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 
 ALLOWED_HOSTS = ['*']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
